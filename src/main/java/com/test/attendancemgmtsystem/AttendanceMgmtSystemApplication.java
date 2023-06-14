13c5dd766c8cbb480323405f41b5bbf4875b7258
Ann Thomas <anntom98@gmail.com>
2023-06-14 16:10:34 +0530

    commiting project Attendance Management System to repository

---
+++ src/main/java/com/test/attendancemgmtsystem/AttendanceMgmtSystemApplication.java
@@ -0,0 +1,13 @@
+package com.test.attendancemgmtsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceMgmtSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceMgmtSystemApplication.class, args);
+	}
+
+}
